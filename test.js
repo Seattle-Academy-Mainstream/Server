2269a3bab1fa62a2a208bebcdc46e190270373e6
Isaac Zinda <isaaczinda@seattleacademy.org>
2015-07-09 16:19:43 -0700

    update test program

--- test.js
+++ test.js
@@ -19,33 +19,91 @@ var Database = mysql.createConnection({
   database : "mainstream"
 });
 
-PostsTable.connect();
+Database.connect();
 
 function SQL_AddPost(DataObject, Callback)
 {
   //add the data to mysql
-    PostsTable.query('INSERT INTO posts SET ?', DataObject, function(err, result) 
+    Database.query('INSERT INTO posts SET ?', DataObject, function(err, result) 
     {
+	if(err)
+	{
+		console.log(err);
+	}
       Callback();
     });
 }
 
-function SQL_ToJSON()
+function SQL_AddUpvote(ID, Username, Callback)
 {
-  var Posts = [];
+	var UpvoteArray = [];
 
-    connection.query('SELECT * FROM posts', function(err, results) 
-    {
-      for (var i = 0; i < results.length; i++)
-      {
-        connection.query('SELECT * FROM upvotes WHERE id = ?', results[i]["ID"], function(err, UpvoteUsers) {
-          results[i]["Upvotes"] = UpvoteUsers;
-        });
-      }
+
+	Database.query('SELECT * FROM upvotes WHERE id = ?', ID, function(err, UpvoteUsers) {
+		for(var p = 0; p < UpvoteUsers.length; p++)
+        	{
+                	UpvoteArray.push(UpvoteUsers[p]["Author"]);
+        	}
+		console.log(UpvoteArray);
+		if(UpvoteArray.indexOf(Username) == -1)
+		{
+			Database.query('INSERT INTO upvotes SET ?', {"ID": ID, "Author": Username}, function(err, result)
+        {
+                if(err)
+                {
+                        console.log(err);
+                }
+        Callback();
     });
 
-  return Posts;
+		}
+		else
+		{
+		Database.query('DELETE FROM upvotes WHERE Author = ?', Username, function (err, result) {
+Callback();
+		
+});	
+		}
+	});
 }
 
-SQL_AddPost({"Category": "Sports", "Content": "Hello World", "Author": "me@isaaczinda.com", "ID": "0123456789"});
-JSON.parse(SQL_ToJSON());
+function SQL_ToJSON(Callback)
+{
+    Database.query('SELECT * FROM posts', function(err, results) 
+    {
+	console.log(results);
+	var timesrun = 0;
+      results.forEach(function(i)
+      {
+        Database.query('SELECT * FROM upvotes WHERE id = ?', i["ID"], function(err, UpvoteUsers) {
+	var UpvoteArray = [];
+	for(var p = 0; p < UpvoteUsers.length; p++)
+	{
+		UpvoteArray.push(UpvoteUsers[p]["Author"]);
+	}          
+
+i["Upvotes"] = UpvoteArray;
+	 timesrun += 1;
+        if(timesrun == results.length)
+        {
+                console.log(timesrun);
+                console.log(results);
+                Callback(results);
+
+        }
+        
+});
+      });
+    });
+}
+SQL_AddUpvote("0123456789", "i@saa.c", function()
+{
+SQL_AddPost({"Image": "test.png", "Category": "Sports", "Content": "Hello World 2", "Author": "me@isaaczinda.com", "ID": "012346789"}, function()
+{
+	SQL_ToJSON(function(data)
+	{
+		console.log(data);
+		console.log(JSON.stringify(data));
+	});
+});
+});
