6b2786477d4b0152f8bd6c4af5348ff2fa5e8fe4
Isaac Benson Zinda <isaaczinda@seattleacademy.org>
2015-07-08 19:58:40 -0700

    add test.js

---
+++ test.js
@@ -0,0 +1,49 @@
+//sets up includes
+var http = require('http');
+var fs = require('fs');
+var settings = require('./settings.json');
+
+//these are the globals
+var Posts = [];
+
+var mysql = require('mysql');
+
+//table names
+//posts, upvotes
+var Database = mysql.createConnection({
+  host     : settings.host,
+  user     : settings.user,
+  password : settings.password, 
+  database : "mainstream"
+});
+
+PostsTable.connect();
+
+function SQL_AddPost(DataObject, Callback)
+{
+  //add the data to mysql
+    PostsTable.query('INSERT INTO posts SET ?', DataObject, function(err, result) 
+    {
+      Callback();
+    });
+}
+
+function SQL_ToJSON()
+{
+  var Posts = [];
+
+    connection.query('SELECT * FROM posts', function(err, results) 
+    {
+      for (var i = 0; i < results.length; i++)
+      {
+        connection.query('SELECT * FROM upvotes WHERE id = ?', results[i]["ID"], function(err, results) {
+
+        });
+      }
+    });
+
+  return Posts;
+}
+
+SQL_AddPost({"Category": "Sports", "Content": "Hello World",, "Author": "me@isaaczinda.com", "ID": "0123456789"});
+JSON.parse(SQL_ToJSON());
