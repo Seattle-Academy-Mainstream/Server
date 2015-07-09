9aa9689c93188038d43520627b9038122e9bc235
Alex Jordan <alex@strugee.net>
2015-07-08 19:40:44 -0700

    Fix SQL script bugs

--- init.sql
+++ init.sql
@@ -1,8 +1,9 @@
-CREATE DATABASE mainstream;
+CREATE DATABASE mainstream IF NOT EXISTS;
 # You should replace "password" below with a real password
 GRANT SELECT, INSERT, UPDATE, DELETE ON mainstream.* TO mainstream@localhost IDENTIFIED BY 'password';
 
 FLUSH PRIVILEGES;
 
+USE mainstream;
 CREATE TABLE `posts` (`id` CHAR(10) NOT NULL, `content` TEXT, `image` TINYTEXT, `author` TINYTEXT, `category` TINYTEXT, PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE `upvotes` (`id` CHAR(10) NOT NULL, `author` VARCHAR(100), FOREIGN KEY (id) REFERENCES posts(id) ON DELETE CASCADE) ENGINE=InnoDB DEFAULT CHARSET=utf8;
