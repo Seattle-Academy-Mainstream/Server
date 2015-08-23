842b5085b01475bf9a809b15de8c29ea1020f921
Isaac Benson Zinda <isaaczinda@seattleacademy.org>
2015-08-23 12:04:02 -0700

    update

--- cropper.js
+++ cropper.js
@@ -1,16 +1,19 @@
 var easyimg = require('easyimage');
 
+var ImageObject = {"scale": 1.77777, "angle": 0, "x": 0, "y": 0, "w": 640, "h": 480};
+
 easyimg.crop({
-     src:'test.png', dst:'cropped-test.png',
-     width:500, height:500,
-     cropwidth:128, cropheight:128,
-      gravity:'North',
-     x:0, y:0
-  }).then(
-  function(image) {
-     console.log('Resized and cropped: ' + image.width + ' x ' + image.height);
-  },
-  function (err) {
-    console.log(err);
-  }
-);
+  src:'testimage.png', dst:'cropped-test.png',
+  width: 640, height: 480,
+  cropwidth: (ImageObject["w"] / ImageObject["scale"]), cropheight: (ImageObject["h"] / ImageObject["scale"]),
+  gravity:'North',
+  x: (ImageObject["x"] / ImageObject["scale"]), y: (ImageObject["h"] / ImageObject["scale"])
+}).then(
+function(image) 
+{
+   console.log('Resized and cropped: ' + image.width + ' x ' + image.height);
+},
+function (err) 
+{
+  console.log(err);
+});
