3c445c9fdc4b8a485ca06ef455185cd2551d7462
Isaac Benson Zinda <isaaczinda@seattleacademy.org>
2015-08-27 15:42:34 -0700

    update

--- cropper.js
+++ cropper.js
@@ -13,13 +13,13 @@ exports.Crop = function(ImageObject, ImageName, Callback)
   //convert flower.jpg -crop 128×128+50+50 flower_crop.jpg
 
   var exec = require('child_process').exec;
-  var Command = "convert " + "/var/local/mainstreamd/RawImages/" + ImageName + " -crop " + Math.floor(640 / ImageObject["scale"]) + "×" + Math.floor(480 / ImageObject["scale"]) + "+" + Math.floor(ImageObject["x"] / ImageObject["scale"]) +"+" + Math.floor(ImageObject["y"] / ImageObject["scale"]) + " /var/local/mainstreamd/Images/" + ImageName;
+  var Command = "convert " + "/var/local/mainstreamd/RawImages/" + ImageName + " -crop " + Math.floor(640 / ImageObject["scale"]) + "x" + Math.floor(480 / ImageObject["scale"]) + "+" + Math.floor(ImageObject["x"] / ImageObject["scale"]) +"+" + Math.floor(ImageObject["y"] / ImageObject["scale"]) + " /var/local/mainstreamd/Images/" + ImageName;
 
   console.log(Command);
 
   exec(Command, function(error, stdout, stderr) {
     console.log(error);
-    
+
     Callback();
   });
 
