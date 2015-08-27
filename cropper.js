70bf574dda321d4f4ebe761b898c1a3a01f6915d
Isaac Benson Zinda <isaaczinda@seattleacademy.org>
2015-08-27 15:04:44 -0700

    update

--- cropper.js
+++ cropper.js
@@ -6,6 +6,9 @@ var exports = module.exports = {};
 //{"scale": 1.77777, "angle": 0, "x": 0, "y": 256, "w": 360, "h": 640}
 exports.Crop = function(ImageObject, ImageName, Callback)
 {
+  console.log("CropSize = " + 640 / ImageObject["scale"] + "x" + ImageObject["y"] / ImageObject["scale"]);
+  console.log("CropStart = " + ImageObject["x"] / ImageObject["scale"] + ", " + ImageObject["y"] / ImageObject["scale"]);
+
   easyimg.crop({
     src: "/var/local/mainstreamd/RawImages/" + ImageName, dst:"/var/local/mainstreamd/Images/" + ImageName,
     cropwidth: (640 / ImageObject["scale"]), cropheight: (480 / ImageObject["scale"]),
